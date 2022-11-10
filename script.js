5ff56aadc705f79510dece78f06b2da5fe950302
Hergelink <enver.hergelink@gmail.com>
2022-11-10 10:09:50 +0300

    added tech stack icons

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gallery = document.getElementById('gallery');
 
 
 
+
 window.onmousemove = (e) => {
   const mouseX = e.clientX,
     mouseY = e.clientY;
@@ -10,22 +11,20 @@ window.onmousemove = (e) => {
   const xDecimal = mouseX / window.innerWidth;
   const yDecimal = mouseY / window.innerHeight;
 
-    
-    const maxX = gallery.offsetWidth - window.innerWidth;
-   const maxY = gallery.offsetHeight - window.innerHeight;
+  const maxX = gallery.offsetWidth - window.innerWidth;
+  const maxY = gallery.offsetHeight - window.innerHeight;
 
   const panX = maxX * xDecimal * -1;
- const panY = maxY * yDecimal * -1;
+  const panY = maxY * yDecimal * -1;
 
- 
-    gallery.animate({
-        transform: `translate(${panX}px, ${panY}px)`
-    }, {
-        duration: 4000,
-        fill: 'forwards',
-        easing: 'ease'
-    })
-    
+  gallery.animate(
+    {
+      transform: `translate(${panX}px, ${panY}px)`,
+    },
+    {
+      duration: 5000,
+      fill: 'forwards',
+      easing: 'ease',
+    }
+  );
 };
-
-
