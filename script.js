7661565deba04490f01656583c05bcbe52d6a00f
Hergelink <enver.hergelink@gmail.com>
2023-02-13 13:55:41 +0300

    added a setinterval function to check screen size every 1 second

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 const gallery = document.getElementById('gallery');
 
+function checkScreenSize() {
+  if (window.innerWidth > 768) {
+    window.onmousemove = (e) => {
+      const mouseX = e.clientX,
+        mouseY = e.clientY;
 
+      const xDecimal = mouseX / window.innerWidth;
+      const yDecimal = mouseY / window.innerHeight;
 
+      const maxX = gallery.offsetWidth - window.innerWidth;
+      const maxY = gallery.offsetHeight - window.innerHeight;
 
+      const panX = maxX * xDecimal * -1;
+      const panY = maxY * yDecimal * -1;
 
-window.onmousemove = (e) => {
-  const mouseX = e.clientX,
-    mouseY = e.clientY;
+      gallery.animate(
+        {
+          transform: `translate(${panX}px, ${panY}px)`,
+        },
+        {
+          duration: 5000,
+          fill: 'forwards',
+          easing: 'ease',
+        }
+      );
+    };
+  } else {
+    return;
+  }
+}
 
-  const xDecimal = mouseX / window.innerWidth;
-  const yDecimal = mouseY / window.innerHeight;
-
-  const maxX = gallery.offsetWidth - window.innerWidth;
-  const maxY = gallery.offsetHeight - window.innerHeight;
-
-  const panX = maxX * xDecimal * -1;
-  const panY = maxY * yDecimal * -1;
-
-  gallery.animate(
-    {
-      transform: `translate(${panX}px, ${panY}px)`,
-    },
-    {
-      duration: 5000,
-      fill: 'forwards',
-      easing: 'ease',
-    }
-  );
-};
+setInterval(checkScreenSize, 1000);
